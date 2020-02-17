20ae3b84187fd91efdf59761741e9b543921c2b8
AtabaevaMadina <atabaevamadina5@gmail.com>
2020-02-17 20:02:47 +0600

    1st commit

---
+++ task10.py
@@ -0,0 +1,20 @@
+str_ = input("Write a string: ")
+a = str_[2]
+print(a)
+b = str_[-2]
+print(b)
+c = str_[:5]
+print(c)
+d = str_[:-2]
+print(d)
+e = str_[0::2]
+print(e)
+f = str_[1::2]
+print(f)
+g = str_[::-1]
+print(g)
+h = str_[-1::-2]
+print(h)
+j = len(str_)
+print(j)
+
